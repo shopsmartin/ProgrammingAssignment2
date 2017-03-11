645e71a72d9ce8f85559b897cc7597597d6797fb
shopsmartin <shopsmartin@gmail.com>
2017-03-11 09:59:51 -0600

    coursera-rprogramming-asnmt2
    caching a matrix and it's inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is for programming assignment 2 to 
+# cache an inverse of a matrix
+#first this function takes and stores the matrix in the corresponding 
+#levels of the environment and returns a list
+#requires an inversable matrix as argument
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(v=rnorm(100), row=10, col=10) {
+    x = matrix(v, row, col)
+    cache <- NULL
 
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solveMatx) cache <<- solveMatx
+    getinverse <- function() cache
+list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## if given result from makeCacheMatrix,
+# than this function gets it's inverse or gets a cached inverse 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cache <- x$getinverse()
+    if (!is.null(cache)) {
+        message("getting cached data")
+        return(cache)
+    }
+    data <- x$get()
+    cache <- solve(data, ...)
+    x$setinverse(cache)
+    cache
+## Return a matrix that is the inverse of 'x'
 }
+
+
+# To test
+mat <- makeCacheMatrix(rnorm(16), 4,4)
+mat$get() #retrieve the matrix
+mat$getinverse() # should be NULL or location in memory equivalent
+
+cacheSolve(mat) #should return the inverse of mat
+
+mat$getinverse() #should now return cached inverted matrix
+
